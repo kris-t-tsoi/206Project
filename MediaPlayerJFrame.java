8a060c1983748b463c6c25d53ebcc322c7fda02f
Stefan Gorgiovski <stefan.gorgiovski@hotmail.com>
2015-09-03 16:43:34 +1200

    created initial UI with no functionality

---
+++ MediaPlayerJFrame.java
@@ -0,0 +1,116 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+
+public class MediaPlayerJFrame extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtInputText;
+	private final int buttonWidth = 125;
+	EmbeddedMediaPlayerComponent mediaPlayerComponent;
+	EmbeddedMediaPlayer video;
+	/**
+	 * Create the frame.
+	 */
+	public MediaPlayerJFrame(String name) {
+		super(name);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 431, 360);
+		contentPane = new JPanel();
+		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JPanel mediaPanel = new JPanel(new BorderLayout());
+		mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+		video = mediaPlayerComponent.getMediaPlayer();
+		mediaPanel.add(mediaPlayerComponent, BorderLayout.CENTER);
+		
+		JButton btnBackward = new JButton("Back");
+		
+		JButton btnPlay = new JButton("Play");
+		btnPlay.setToolTipText("Play the video");
+		
+		JButton btnForward = new JButton("Forward");
+		
+		txtInputText = new JTextField();
+		txtInputText.setToolTipText("Text to synthesize here - max 30 words");
+		txtInputText.setText("Text to synthesize here - max 30 words");
+		txtInputText.setColumns(10);
+		
+		JButton btnPlayText = new JButton("Play text");
+		btnPlayText.setToolTipText("Listen to the text");
+		
+		JButton btnSaveText = new JButton("Save text");
+		btnSaveText.setToolTipText("Save the text to a mp3 file");
+		
+		JButton btnSelectMp3 = new JButton("Select mp3");
+		btnSelectMp3.setToolTipText("Select an mp3 to add to the start of the video\r\n");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(mediaPanel, GroupLayout.DEFAULT_SIZE, 469, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnBackward, GroupLayout.PREFERRED_SIZE, buttonWidth, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnPlay, GroupLayout.PREFERRED_SIZE, buttonWidth, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnForward, GroupLayout.PREFERRED_SIZE, buttonWidth, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(145, Short.MAX_VALUE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(txtInputText, GroupLayout.DEFAULT_SIZE, 445, Short.MAX_VALUE)
+					.addContainerGap())
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnPlayText, GroupLayout.PREFERRED_SIZE, buttonWidth, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnSaveText, GroupLayout.PREFERRED_SIZE, buttonWidth, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnSelectMp3, GroupLayout.PREFERRED_SIZE, buttonWidth, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(145, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(mediaPanel, GroupLayout.DEFAULT_SIZE, 247, Short.MAX_VALUE)
+					.addGap(10)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnBackward, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnPlay, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnForward, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtInputText, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnPlayText, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnSaveText, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnSelectMp3, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap())
+		);
+		contentPane.setLayout(gl_contentPane);
+		
+		setVisible(true);
+		
+	}
+	public void release() {
+		mediaPlayerComponent.release();
+	}
+	
+	public void play() {
+		video.playMedia("big_buck_bunny_1_minute.avi");
+	}
+}
--- test.java
+++ test.java
@@ -1,24 +1,19 @@
-import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-
-import uk.co.caprica.vlcj.binding.LibVlc;
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
 import uk.co.caprica.vlcj.discovery.NativeDiscovery;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 
 public class test {
 	
-	private final JFrame frame;
+	private final MediaPlayerJFrame frame;
 	
-	private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
-	
-	
+	private EmbeddedMediaPlayerComponent mediaPlayerComponent;
 	
 	public static void main(String args[]) {
-		boolean found = new NativeDiscovery().discover();
+		//boolean found = new NativeDiscovery().discover();
 		
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
@@ -29,22 +24,22 @@ public class test {
 	}
 	
 	public test() {
-		frame = new JFrame("A media player");
+		frame = new MediaPlayerJFrame("Vidivox");
 		frame.setBounds(100,100,600,400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		frame.addWindowListener(new WindowAdapter() {;
 			@Override
 			public void windowClosing(WindowEvent e) {
-				mediaPlayerComponent.release();
+				frame.release();
 				System.exit(0);
 			}
 		});
 		
-		mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
-		frame.setContentPane(mediaPlayerComponent);
-		frame.setVisible(true);
-		mediaPlayerComponent.getMediaPlayer().playMedia(System.getProperty("user.dir") + "/sample.mp4");
+		
+		frame.play();
+		//frame.addVideo(mediaPlayerComponent);
+		
 		
 	}
 	
