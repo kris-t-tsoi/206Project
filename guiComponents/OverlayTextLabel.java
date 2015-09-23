34ba0d655be57ef4ede950bf4c113aac03729418
Kristy <kris.t.tsoi@gmail.com>
2015-09-23 22:38:30 +1200

    added untracked files, pulling from repository

---
+++ guiComponents/OverlayTextLabel.java
@@ -0,0 +1,12 @@
+package guiComponents;
+
+import main.MediaPlayerJFrame;
+
+public class OverlayTextLabel extends AbstractMediaLabel{
+
+	public OverlayTextLabel(MediaPlayerJFrame parentFrame) {
+		super(parentFrame);
+		setText("Text");
+	}
+
+}
