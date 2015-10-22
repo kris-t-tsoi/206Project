0c6356d7307d680ccc9dfd1042ab5e0cc0bbf6f9
kris-t-tsoi <kris.t.tsoi@gmail.com>
2015-10-23 01:40:27 +1300

    overfix textbox error

--- doInBackground/progress/ProgressFrame.java
+++ doInBackground/progress/ProgressFrame.java
@@ -1,5 +1,7 @@
 package doInBackground.progress;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -16,17 +18,15 @@ public class ProgressFrame extends JFrame {
 	JPanel contentPane;
 	JLabel percentProgress;
 	JProgressBar processBar;
-	Timer time;
 
 	public ProgressFrame() {
 		super("Working In Background");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(900, 400, 250, 150);
+		setBounds(850, 350, 250, 150);
 		setVisible(true);
 
 		setResizable(false);
-		
-		
+	
 		
 		processBar = new JProgressBar(JProgressBar.HORIZONTAL);
 		processBar.setIndeterminate(true);
@@ -39,12 +39,12 @@ public class ProgressFrame extends JFrame {
 		contentPane
 				.setLayout(new MigLayout(
 						"", // Layout Constraint
-						"[5px,grow 0,shrink 0][120px,grow 0, shrink 0][5px,grow 0,shrink 0]", // Column
+						"[5px,grow 0,shrink 0][120px,grow, shrink][5px,grow 0,shrink 0]", // Column
 																								// Constraints
 						"[5px][100px][10px][100px][5px]")); // Row Constraints
 
-		contentPane.add(nameLbl, "cell 1 1, alignx center");
-		contentPane.add(processBar, "cell 1 3, alignx center");
+		contentPane.add(nameLbl, "cell 1 1, alignx center,grow");
+		contentPane.add(processBar, "cell 1 3, alignx center,grow");
 		
 		setVisible(true);
 
--- textToMP3Frame/textBoxAndSliders/TextToMP3TextBox.java
+++ textToMP3Frame/textBoxAndSliders/TextToMP3TextBox.java
@@ -9,52 +9,49 @@ import doInBackground.UseTerminalCommands;
 @SuppressWarnings("serial")
 public class TextToMP3TextBox extends JTextArea {
 	public static final int MAX_NUMBER_OF_WORDS = 50;
-	
-	
+
 	public TextToMP3TextBox() {
 		super("Text to synthesize here - max 50 words");
 		setToolTipText("Type in Text you Wish to Hear as Audio");
-		
-		//if sentence is too long the whole word will go onto the next line
+
+		// if sentence is too long the whole word will go onto the next line
 		setLineWrap(true);
 		setWrapStyleWord(true);
 	}
 
-	
 	/**
 	 * Function to check if the number of words a string of text exceeds a
 	 * certain value. Used before reading or saving any text.
 	 * 
-	 * @param text - from textField
-	 * @return 	true if number of words is less than 30, 
-	 * 			false if number of words is greater than 30
+	 * @param text
+	 *            - from textField
+	 * @return true if number of words is less than 30, false if number of words
+	 *         is greater than 30
 	 * 
 	 */
 	public boolean checkTxtLength() {
-		
-		//if it is blank
-		if((getText() != null) && !getText().startsWith(" ")){
-			return false;
-		}
-		
-		// Removes all spaces and punctuation apart from ' for conjunctions
-		String[] punct = getText().split("[^a-zA-Z0-9']");
-		int words = 0;
-		for (int i = 0; i < punct.length; i++) {
-			if (!punct[i].equals("")) {
-				words++;
+
+		// if it is blank
+		if ((getText() != null) && !getText().startsWith(" ")) {
+
+			// Removes all spaces and punctuation apart from ' for conjunctions
+			String[] punct = getText().split("[^a-zA-Z0-9']");
+			int words = 0;
+			for (int i = 0; i < punct.length; i++) {
+				if (!punct[i].equals("")) {
+					words++;
+				}
 			}
-		}
-		if (words <= MAX_NUMBER_OF_WORDS) {
-			return true;
+			if (words <= MAX_NUMBER_OF_WORDS) {
+				return true;
+			}
 		}
 		return false;
 	}
-	
 
-	//TODO set mindimension
+	// TODO set mindimension
 	@Override
 	public Dimension getMinimumSize() {
-		return new Dimension(0,0);
+		return new Dimension(0, 0);
 	}
 }
