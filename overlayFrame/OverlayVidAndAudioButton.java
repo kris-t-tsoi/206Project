2633b41d4b53c970462041f2da0a7c57cd3e2df2
kris-t-tsoi <kris.t.tsoi@gmail.com>
2015-10-24 00:06:23 +1300

    overlay mulitple audio

--- overlayFrame/OverlayVidAndAudioButton.java
+++ overlayFrame/OverlayVidAndAudioButton.java
@@ -29,6 +29,7 @@ public class OverlayVidAndAudioButton extends JButton {
 
 		String ffmpegVideoPath = "ffmpeg -i " + video.getVideoPath() + " ";
 		String ffmpegMP3Paths = "";
+		String ffmpegDelay = "";
 		String ffmpegMediaNumAndChannel = "";
 
 		int count = 1;
@@ -41,36 +42,35 @@ public class OverlayVidAndAudioButton extends JButton {
 
 			// add in paths of the audio files
 			ffmpegMP3Paths = ffmpegMP3Paths + "-i " + audio.getPath() + " ";
-			ffmpegMediaNumAndChannel = ffmpegMediaNumAndChannel + "[" + count+ ":a]adelay=" + delay;
+			ffmpegDelay = ffmpegDelay + "[" + count+ ":a]adelay=" + delay+"[a"+count+"]";
+			ffmpegMediaNumAndChannel = ffmpegMediaNumAndChannel +"[a"+count+"]";
 			
 			//if it is not the last audio add a semicolon
 			if (count != listAudio.size()) {
-				ffmpegMediaNumAndChannel = ffmpegMediaNumAndChannel + ";";
+				ffmpegDelay = ffmpegDelay + ";";
 			}
 			
 			count++;
 		}
 
 		@SuppressWarnings("static-access")
-		String ffmpegAmix = ",amix=inputs=" + count + "\" "
+		String ffmpegAmix = "amix=inputs=" + count + "\" "
 				+ video.VIDEO_DIR_RELATIVE_PATH + File.separator + outName
 				+ ".mp4";
 
 		System.out.println("ffmpeg " + ffmpegVideoPath);
 		System.out.println("ffmpeg " + ffmpegMP3Paths);
-		System.out.println("ffmpeg " + ffmpegMediaNumAndChannel);
+		System.out.println("ffmpeg " + ffmpegDelay);
 		System.out.println("ffmpeg " + ffmpegAmix);
 
 		String cmd = ffmpegVideoPath + ffmpegMP3Paths + "-filter_complex \""
-				+ ffmpegMediaNumAndChannel + ffmpegAmix;
+				+ ffmpegDelay +","+ffmpegMediaNumAndChannel+ ffmpegAmix;
 		System.out.println(cmd);
 
 		CreateInBackground back = new CreateInBackground(cmd);
 		back.execute();
 
-		// ffmpeg -i Video/big_buck_bunny_1_minute.avi -i MP3/haehah.mp3
-		// -filter_complex [media
-		// number:channel]adelay=delayinMilisec4,amix=inputs=2 out.mp4
+		// ffmpeg -i Video/big_buck_bunny_1_minute.avi  -i MP3/pleasework.mp3 -i MP3/yery.mp3 -filter_complex "[1:a]adelay=200[a1];[2:a]adelay=10000[a2],[a1][a2]amix=inputs=3" Video/lll.mp4
 
 		//TODO return path file ?
 		return "";
